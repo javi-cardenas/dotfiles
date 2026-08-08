f59518ad40bb4020b95d5c3a296086fd237180e1
Javi Cardenas <85462046+javi-cardenas@users.noreply.github.com>
2026-08-08 12:02:37 -0400

    fix: add setup script (#13)

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+script_dir=$(CDPATH= cd "$(dirname "$0")" && pwd)
+repo_dir=$(dirname "$script_dir")
+
+cd "$repo_dir"
+"$script_dir/brew.sh"
+just stow
+just finder
